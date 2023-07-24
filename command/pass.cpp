26b5469e38bf81547bcd34cdde10e5fe26fdecf8
Seunghun Song <seunghso@c4r1s3.42seoul.kr>
2023-07-24 19:48:52 +0900

    to merge

--- command/pass.cpp
+++ command/pass.cpp
@@ -4,15 +4,22 @@
 int checkPassword(std::string pass, int clientId)
 {
 	Server& server = Server::getInstance();
-	int passflag = 0;
+	Client* clients = server.getClients();
+
+	if (clients[clientId].getPassFlag())
+	{
+		return 462;
+	}
+
+ 	int passflag = 0;
 	if (pass.c_str() != NULL)
 	{
 		if (std::strncmp(server.getGenernalPass().c_str(), pass.c_str(), server.getGenernalPass().size() + 1) == 0)
 		{
 			std::cout << "The password is correct" << std::endl;
-			(server.getClients()[clientId]).setAdminFlag(false);
+			clients[clientId].setAdminFlag(false);
 		}
-		else if (std::strncmp(server.getGenernalPass().c_str(), pass.c_str(), server.getGenernalPass().size() + 1) == 0)
+		else if (std::strncmp(server.getOperatorPass().c_str(), pass.c_str(), server.getOperatorPass().size() + 1) == 0)
 		{
 			std::cout << "The password is correct (Operator)" << std::endl;
 			(server.getClients()[clientId]).setAdminFlag(true);
