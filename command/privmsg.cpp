e66a626cc25082b0de8c4a7b63fd301edeeb81ce
mkwon <mkwon@student.42seoul.kr>
2023-07-25 15:26:44 +0900

    feat: make PRIVMSG sendUser

--- command/privmsg.cpp
+++ command/privmsg.cpp
@@ -1,17 +1,45 @@
 #include "command.hpp"
 #include "../Channel.hpp"
 
-// void sendUser(int fd, std::string str)
-// {
-// 	int numeric;
-// 	std::string message;
+void sendUser(int fd, std::string str)
+{
+	int numeric;
+	std::string message;
 
-// 	Server& server = Server::getInstance();
-// 	Client* clients = server.getClients();
+	Server& server = Server::getInstance();
+	Client* clients = server.getClients();
 
-// 	// , 단위로 split
-// 	// clients돌면서 PRIV보내기
-// }
+	// , 단위로 split
+	// clients돌면서 PRIV보내기
+	size_t spacePoint = str.find(' ');
+	std::string userNick = str.substr(0, spacePoint);
+
+	int nickNameId = server.getNickNameId(userNick);
+	if (nickNameId == -1)
+	{
+		numeric = ERR_NOSUCHNICK;
+		message += " ";
+		message += clients[fd].getNickName();
+		message += " ";
+		message += userNick;
+        message += " :NO such nick";
+		server.sendMessage(fd, (std::to_string(numeric) + message));
+        return ;
+	}
+
+	size_t messagePoint = str.find(':');
+	std::string chatMessage = str.substr(messagePoint);
+
+	numeric = RPL_AWAY;
+	message += " ";
+	message += clients[fd].getNickName();
+	message += " PRIVMSG ";
+	message += userNick;
+	message += " :";
+	message += chatMessage;
+	
+	server.sendMessage(nickNameId, (std::to_string(numeric) + message));
+}
 
 void sendChannel(int fd, std::string str, size_t chennelPoint)
 {
@@ -36,8 +64,10 @@ void sendChannel(int fd, std::string str, size_t chennelPoint)
 			continue;
 		
 		numeric = RPL_AWAY;
-		message += " PRIVMSG ";
+		message += " ";
 		message += clients[fd].getNickName();
+		message += " PRIVMSG #";
+		message += channelName;
 		message += " :";
 		message += chatMessage;
 		if (i != fd && clientStatus[i] == CONNECTED)
@@ -48,8 +78,8 @@ void sendChannel(int fd, std::string str, size_t chennelPoint)
 void PRIVMSG(int fd, std::string str)
 {
 	size_t chennelPoint = str.find('#');
-	// if (chennelPoint == std::string::npos)
-	// 	sendUser();
-	// else
+	if (chennelPoint == std::string::npos)
+		sendUser();
+	else
 		sendChannel(fd, str, chennelPoint);
 }
